307117393f556c697644b6fa6807b95873c21d67
tuanpm3435 <manhtuan6476@gmail.com>
2022-06-17 22:11:12 +0700

    add environment

--- HyperShoot/Assets/Script/Combat/EnemyDamageHandler.cs
+++ HyperShoot/Assets/Script/Combat/EnemyDamageHandler.cs
@@ -6,5 +6,39 @@ namespace HyperShoot.Combat
 {
     public class EnemyDamageHandler : DamageHandler
     {
+        [System.Serializable]
+        public class ItemRate
+        {
+            public GameObject item;
+            public float rate;
+
+            public ItemRate(GameObject item, float rate)
+            {
+                this.item = item;
+                this.rate = rate;
+            }
+        }
+
+        [SerializeField] private List<ItemRate> itemRates;
+        public override void Die()
+        {
+            base.Die();
+            List<ItemRate> temp = new List<ItemRate>();
+            float rate = 0;
+            for (int i = 0; i < itemRates.Count; i++)
+            {
+                rate += itemRates[i].rate * 10f;
+                temp.Add(new ItemRate(itemRates[i].item, rate));
+            }
+            int random = Random.Range(0, 1001);
+            for (int i = 0; i < temp.Count; i++)
+            {
+                if (random <= temp[i].rate)
+                {
+                    SimplePool.Spawn(temp[i].item, transform.position, Quaternion.identity);
+                    return;
+                }
+            }
+        }
     }
 }
